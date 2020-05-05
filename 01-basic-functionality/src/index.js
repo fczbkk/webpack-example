5f250a1126c709a3c146e5f0a6ccfac83dad0457
fczbkk <riki@fczbkk.com>
2020-05-05 14:11:48 +0200

    add content and examples

--- 01-basic-functionality/src/index.js
+++ 01-basic-functionality/src/index.js
@@ -1,8 +1,8 @@
 // export/import syntax used by ES6
-import { doSomething } from './library1'
+import { doSomething } from './utilities1'
 
 // CommonJS (CJS) syntax used by NodeJS
-const { doSomethingElse } = require('./library2')
+const { doSomethingElse } = require('./utilities2')
 
 // both kinds of syntax work
 doSomething()
